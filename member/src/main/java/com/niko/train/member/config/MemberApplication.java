4e91dbe226c0a32f39b4cf07068a104c53f1f0a5
niko <1142521580@qq.com>
2025-02-17 20:12:13 +0800

    4-4 修改启动日志，增加/member，修改logback

--- member/src/main/java/com/niko/train/member/config/MemberApplication.java
+++ member/src/main/java/com/niko/train/member/config/MemberApplication.java
@@ -1,15 +1,22 @@
 package com.niko.train.member.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.niko")
 public class MemberApplication {
+	private static final Logger Log = LogManager.getLogger(MemberApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+		SpringApplication app = new SpringApplication(MemberApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		Log.info("启动成功");
+		Log.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }

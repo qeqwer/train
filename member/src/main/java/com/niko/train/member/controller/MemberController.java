588b9d5e6db67cea4437622d3a200a46edb67e99
niko <1142521580@qq.com>
2025-02-20 00:03:14 +0800

    4-19 集成校验框架validation

--- member/src/main/java/com/niko/train/member/controller/MemberController.java
+++ member/src/main/java/com/niko/train/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.niko.train.common.resp.CommonResp;
 import com.niko.train.member.req.MemberRegisterReq;
 import com.niko.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
         Long register = memberService.register(req);
         return new CommonResp<>(register);
     }

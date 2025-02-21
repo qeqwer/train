029d8ea6527185fa2203f3a21dd2fa4d13edb273
niko <1142521580@qq.com>
2025-02-21 18:57:17 +0800

    6.6 vuex配合 h5 session缓存，解决浏览器刷新丢失数据的问题

---
+++ web/public/js/session-storage.js
@@ -0,0 +1,24 @@
+/**
+ * 封装sessionStorage为SessionStorage
+ * （sessionStorage只能操作字符串，故需封装）
+ */
+
+// sessionStorage，会话缓存，浏览器关闭失效；
+// LocalStorage,浏览器关闭后重新打开有效
+SessionStorage = {
+    get: function (key) {
+        var v = sessionStorage.getItem(key);
+        if (v && typeof(v) !== "undefined" && v !== "undefined") {
+            return JSON.parse(v);
+        }
+    },
+    set: function (key, data) {
+        sessionStorage.setItem(key, JSON.stringify(data));
+    },
+    remove: function (key) {
+        sessionStorage.removeItem(key);
+    },
+    clearAll: function () {
+        sessionStorage.clear();
+    }
+};

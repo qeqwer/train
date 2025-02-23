3267162067aefc07f3651500a94e25229b118de9
niko <1142521580@qq.com>
2025-02-23 11:42:18 +0800

    7.5 使用线程本地变量存储会员信息（拦截器）

---
+++ member/src/main/java/com/niko/train/member/config/SpringMvcConfig.java
@@ -0,0 +1,27 @@
+package com.niko.train.member.config;
+
+import com.niko.train.common.interceptor.MemberInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    MemberInterceptor memberInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        // 路径不要包含context-path
+        registry.addInterceptor(memberInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/member/hello",
+                        "/member/member/send-code",
+                        "/member/member/login"
+                );
+    }
+}

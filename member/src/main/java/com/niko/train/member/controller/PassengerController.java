bb7231d3abf68a240fc061711ca4b3421def46d9
niko <1142521580@qq.com>
2025-02-25 00:06:37 +0800

    7.8 乘车人列表查询接口，只能查看当前会员自己添加的乘客

--- member/src/main/java/com/niko/train/member/controller/PassengerController.java
+++ member/src/main/java/com/niko/train/member/controller/PassengerController.java
@@ -1,14 +1,16 @@
 package com.niko.train.member.controller;
 
+import com.niko.train.common.context.LoginMemberContext;
 import com.niko.train.common.resp.CommonResp;
+import com.niko.train.member.req.PassengerQueryReq;
 import com.niko.train.member.req.PassengerSaveReq;
+import com.niko.train.member.resp.PassengerQueryResp;
 import com.niko.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -21,6 +23,13 @@ public class PassengerController {
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req){
         passengerService.save(req);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
     }
 
 }
--- member/src/main/java/com/niko/train/member/service/PassengerService.java
+++ member/src/main/java/com/niko/train/member/service/PassengerService.java
@@ -2,15 +2,21 @@ package com.niko.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.niko.train.common.context.LoginMemberContext;
 import com.niko.train.common.util.SnowUtil;
 import com.niko.train.member.domain.Passenger;
+import com.niko.train.member.domain.PassengerExample;
 import com.niko.train.member.mapper.PassengerMapper;
+import com.niko.train.member.req.PassengerQueryReq;
 import com.niko.train.member.req.PassengerSaveReq;
+import com.niko.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -27,6 +33,17 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+
+    public List<PassengerQueryResp> queryList(PassengerQueryReq req) {
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if(ObjectUtil.isNotNull(req.getMemberId())) {
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+
     }
 
 

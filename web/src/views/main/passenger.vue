4453909f14c88b27b697ab8d0f4e30ebe02fd18b
niko <1142521580@qq.com>
2025-02-24 19:22:02 +0800

    7.7 乘车人新增界面开发，增加【新增】按钮

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,9 +1,25 @@
 <script setup>
+import { ref } from 'vue';
 
+const open = ref(false);
+const showModal = () => {
+  open.value = true;
+};
+const handleOk = e => {
+  console.log(e);
+  open.value = false;
+};
 </script>
 
 <template>
-  <div>乘车人管理</div>
+  <div>
+    <a-button type="primary" @click="showModal">新增</a-button>
+    <a-modal v-model:open="open" title="乘车人" @ok="handleOk">
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </a-modal>
+  </div>
 </template>
 
 <style scoped>

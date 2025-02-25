d813428c02053f5345ff1a8bc01007f02d2b5dde
niko <1142521580@qq.com>
2025-02-25 11:20:19 +0800

    7.9 封装分页返回结果

--- member/src/main/java/com/niko/train/member/controller/PassengerController.java
+++ member/src/main/java/com/niko/train/member/controller/PassengerController.java
@@ -2,6 +2,7 @@ package com.niko.train.member.controller;
 
 import com.niko.train.common.context.LoginMemberContext;
 import com.niko.train.common.resp.CommonResp;
+import com.niko.train.common.resp.PageResp;
 import com.niko.train.member.req.PassengerQueryReq;
 import com.niko.train.member.req.PassengerSaveReq;
 import com.niko.train.member.resp.PassengerQueryResp;
@@ -9,8 +10,6 @@ import com.niko.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -26,10 +25,10 @@ public class PassengerController {
     }
 
     @GetMapping("/query-list")
-    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+    public CommonResp<PageResp<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
         req.setMemberId(LoginMemberContext.getId());
-        List<PassengerQueryResp> list = passengerService.queryList(req);
-        return new CommonResp<>(list);
+        PageResp<PassengerQueryResp> pagesResp =  passengerService.queryList(req);
+        return new CommonResp<>(pagesResp);
     }
 
 }

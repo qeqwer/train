ef28a69d6cd647e7126f1b271b832c8500e70fce
niko <1142521580@qq.com>
2025-02-27 12:41:02 +0800

    8.10 制作queryResp.ftl模板

--- generator/src/main/java/com/niko/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/niko/train/generator/server/ServerGenerator.java
@@ -76,7 +76,9 @@ public class ServerGenerator {
 
 //        gen(Domain, param, "service","service");
 //        gen(Domain, param, "controller","controller");
-        gen(Domain, param, "req", "saveReq");
+//        gen(Domain, param, "req", "saveReq");
+        gen(Domain, param, "req", "queryReq");
+        gen(Domain, param, "resp", "queryResp");
     }
 
     private static void gen(String Domain, Map<String, Object> param, String packageName, String target)

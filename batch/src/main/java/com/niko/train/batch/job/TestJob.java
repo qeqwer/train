1f83bec0a9ca2932afd167b8690f7abaece67282
niko <1142521580@qq.com>
2025-03-02 22:36:30 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/niko/train/batch/job/TestJob.java
+++ batch/src/main/java/com/niko/train/batch/job/TestJob.java
@@ -1,14 +1,21 @@
 package com.niko.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-
+@DisallowConcurrentExecution // 禁用并发执行
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("TestJob");
+        System.out.println("TestJob开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob结束");
     }
 }

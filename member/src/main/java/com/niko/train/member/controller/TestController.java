6d75f72feef101defce60f91810cad38e2dcd3bc
niko <1142521580@qq.com>
2025-03-11 14:34:40 +0800

    13.8 配置gateway+Nacos，实现按应用名来做路由转发

--- member/src/main/java/com/niko/train/member/controller/TestController.java
+++ member/src/main/java/com/niko/train/member/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.niko.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,12 @@ public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
 
+    @Autowired
+    Environment env;
+
     @GetMapping("/hello")
     public String hello(){
-        return String.format("hello %s", testNacos);
+        String port = env.getProperty("local.server.port");
+        return String.format("hello %s! 端口: %s", testNacos, port);
     }
 }

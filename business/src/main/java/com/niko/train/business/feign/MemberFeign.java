b8adac0d5a7227573b203cf7d98a01901aa5a07a
niko <1142521580@qq.com>
2025-03-11 15:15:20 +0800

    13.9 配置feign+nacos实现按应用名做远程调用

--- business/src/main/java/com/niko/train/business/feign/MemberFeign.java
+++ business/src/main/java/com/niko/train/business/feign/MemberFeign.java
@@ -7,10 +7,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "member", url = "http://127.0.0.1:8001/member")
+@FeignClient("member")
+//@FeignClient(name = "member", url = "http://127.0.0.1:8001/member")
 public interface MemberFeign {
 
-    @GetMapping("/feign/ticket/save")
+    @GetMapping("/member/feign/ticket/save")
     CommonResp<Object> save(@Valid @RequestBody MemberTicketReq req);
 
 

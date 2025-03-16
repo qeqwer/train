cc56a35dc19ed670f9d15b4720aa0c08d9c7acda
niko <1142521580@qq.com>
2025-03-16 19:06:08 +0800

    17.6 Sentinel+nacos实现限流规则持久化

--- business/src/main/java/com/niko/train/business/controller/ConfirmOrderController.java
+++ business/src/main/java/com/niko/train/business/controller/ConfirmOrderController.java
@@ -1,10 +1,15 @@
 package com.niko.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.niko.train.business.req.ConfirmOrderDoReq;
 import com.niko.train.business.service.ConfirmOrderService;
+import com.niko.train.common.exception.BussinessExceptionEnum;
 import com.niko.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/confirm-order")
 public class ConfirmOrderController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ConfirmOrderController.class);
+
     @Resource
     private ConfirmOrderService confirmOrderService ;
 
+    @SentinelResource(value = "confirmOrderDo", blockHandler = "doConfirmBlock")
     @PostMapping("/do")
     public CommonResp<Object> doConfirm(@Valid @RequestBody ConfirmOrderDoReq req){
         confirmOrderService.doConfirm(req);
         return new CommonResp<>();
+    }
+
+
+    /**
+     * 降级方法
+     *
+     * @param req
+     * @param e
+     */
+    public CommonResp<Object> doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+        LOG.info("ConfirmOrderController购票请求被限流：{}", req);
+        //throw new BusinessException(BussinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
+        CommonResp<Object> commonResp = new CommonResp<>();
+        commonResp.setSuccess(false);
+        commonResp.setMessage(BussinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION.getDesc());
+        return commonResp;
     }
 
 
